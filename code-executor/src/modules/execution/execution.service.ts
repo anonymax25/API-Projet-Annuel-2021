6755a98ef80d0e67f99ca0f870df25cf71b1a8c9
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-06-07 18:16:09 +0200

    edit execution results

--- code-executor/src/modules/execution/execution.service.ts
+++ code-executor/src/modules/execution/execution.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from "fs";
-import { spawn } from 'child_process';
+import * as https from "https";
+
+import { spawn, exec } from 'child_process';
+import { Result } from './entity/result';
+import { Languages } from './entity/languages.enum';
 import { Extensions } from '../../entity/extensions.enum';
 import { RunCommand } from '../../entity/run-command.enum';
 import { CodeExecution } from './entity/code-execution';
-import { Result } from './entity/result';
+import { S3Service } from './s3.service';
 
 @Injectable()
 export class ExecutionService {
+
+    constructor(public s3Service: S3Service){}
 
     async runPython(message: CodeExecution): Promise<Result> {
         
@@ -22,7 +28,6 @@ export class ExecutionService {
     }
     
     async runJavascript(message: CodeExecution): Promise<Result> {
-        
         const formatStderr = (stderr: string) => {
             let tmp = stderr.split('\n');
             tmp.splice(0,1);
@@ -33,24 +38,26 @@ export class ExecutionService {
     }
 
 
-    runCode(codeExecution: CodeExecution, formatStderr: (stderr: string) => string): Promise<Result> {
-        
-        const filename = `${codeExecution.name}-script.${Extensions[codeExecution.language]}`
+    async runCode(codeExecution: CodeExecution, formatStderr: (stderr: string) => string): Promise<Result> {
+
+
+        const filename = `./code/${codeExecution.name}-script.${Extensions[codeExecution.language]}`
+
+        let code = codeExecution.code
+        fs.writeFileSync(filename, code)
     
-        fs.writeFileSync(filename, codeExecution.code)
-    
-        let stdout;
-        let stderr;
+        let stdout = [];
+        let stderr = [];
 
         const startExecution = Date.now()
         const node = spawn(RunCommand[codeExecution.language], [filename]);
 
         node.stdout.on('data', function (data) {
-            stdout = data.toString();
+            stdout.push(data.toString());
         });
         
         node.stderr.on('data', function (data) {
-            stderr = data.toString();
+            stderr.push(data.toString());
         });
     
         return new Promise((resolve, reject) => {
@@ -60,8 +67,8 @@ export class ExecutionService {
 
                 let result: Result = {
                     code,
-                    stdout,
-                    stderr,
+                    stdout: stdout.join(""),
+                    stderr: stderr.join(""),
                     executionTime: closeExecution - startExecution
                 }
 
