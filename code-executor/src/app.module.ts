fdc5a0efd1cb1c57107a7872e2c0dcd2562f3cd4
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-07-26 10:38:48 +0200

    edit ci

--- code-executor/src/app.module.ts
+++ code-executor/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExecutionModule } from './modules/execution/execution.module';
 
@@ -7,7 +6,7 @@ import { ExecutionModule } from './modules/execution/execution.module';
   imports: [
     ExecutionModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
